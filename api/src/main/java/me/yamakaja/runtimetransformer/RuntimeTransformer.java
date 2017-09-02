3077d85ea0354db0277ad1917d53a94e24dc7d29
Yamakaja <dastw@gmx.net>
2017-09-02 02:17:53 +0200

    Load tools.jar at runtime

--- api/src/main/java/me/yamakaja/runtimetransformer/RuntimeTransformer.java
+++ api/src/main/java/me/yamakaja/runtimetransformer/RuntimeTransformer.java
@@ -1,79 +1,32 @@
 package me.yamakaja.runtimetransformer;
 
-import com.sun.tools.attach.AgentInitializationException;
-import com.sun.tools.attach.AgentLoadException;
-import com.sun.tools.attach.AttachNotSupportedException;
-import com.sun.tools.attach.VirtualMachine;
-
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.lang.management.ManagementFactory;
-
-import me.yamakaja.runtimetransformer.agent.Agent;
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
 
 /**
  * Created by Yamakaja on 19.05.17.
  */
 public class RuntimeTransformer {
 
-    private Class<?>[] transformers;
-
     public RuntimeTransformer(Class<?>... transformers) {
-        this.transformers = transformers;
+        URLClassLoader urlClassLoader = (URLClassLoader) ClassLoader.getSystemClassLoader();
 
-        File agentFile = saveAgentJar();
-        attachAgent(agentFile);
-    }
-
-    private void attachAgent(File agentFile) {
-            try {
-                String pid = ManagementFactory.getRuntimeMXBean().getName();
-                VirtualMachine vm = VirtualMachine.attach(pid.substring(0, pid.indexOf('@')));
-                vm.loadAgent(agentFile.getAbsolutePath());
-                vm.detach();
-
-                Agent.getInstance().process(transformers);
-            } catch (AttachNotSupportedException | IOException | AgentLoadException | AgentInitializationException e) {
-                e.printStackTrace();
-            }
-    }
-
-    private File saveAgentJar() {
-        File agentFile = null;
-
-        OutputStream out = null;
-        InputStream in = null;
         try {
-            agentFile = File.createTempFile("agent", ".jar");
-            agentFile.deleteOnExit();
+            Method method = URLClassLoader.class.getDeclaredMethod("addURL", URL.class);
+            method.setAccessible(true);
 
-            out = new FileOutputStream(agentFile);
-            in = RuntimeTransformer.class.getResourceAsStream("/agent.jar");
+            File file = new File(new File(System.getProperty("java.home")).getParent(), "lib/tools.jar");
+            if (!file.exists())
+                throw new RuntimeException("Not running with JDK!");
 
-            byte[] buffer = new byte[1024];
-            int count;
-
-            while ((count = in.read(buffer)) > 0)
-                out.write(buffer, 0, count);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (in != null) {
-                try {
-                    in.close();
-                } catch (IOException e) {}
-            }
-
-            if (out != null) {
-                try {
-                    out.close();
-                } catch (IOException e) {}
-            }
+            method.invoke(urlClassLoader, file.toURI().toURL());
+        } catch (Throwable throwable) {
+            throwable.printStackTrace();
         }
 
-        return agentFile;
+        TransformerUtils.attachAgent(TransformerUtils.saveAgentJar(), transformers);
     }
+
 }
